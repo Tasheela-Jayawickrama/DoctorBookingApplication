8e3dfe187ebe23d9b53d40417f0072df5c54dbbb
Tashee <tasheelajayawickrama1999@gmail.com>
2022-12-25 22:21:39 +0530

    2022-12-12 Project Created And Environtment Created

---
+++ resources/views/place-appointment-page.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6 col-md-12 col-sm-12">hello</div>
+            <div class="col-12 col-lg-6 col-md-12 col-sm-12"></div>
+            <div class="col-12 col-lg-6 col-md-12 col-sm-12"></div>
+        </div>
+    </div>
+@endsection
+
+
+@push('css')
+    <style>
+
+    </style>
+@endpush
